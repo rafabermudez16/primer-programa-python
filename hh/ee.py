5884b7824410ebc989217abf90d978b3490ed5c7
rafabermudez16 <38160762+rafabermudez16@users.noreply.github.com>
2018-04-07 13:35:17 -0500

    entrada y salida de datos python
    prog 1

---
+++ hh/ee.py
@@ -0,0 +1,172 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> edad='18'
+>>> nombre='rafa'
+>>> print("hola",nombre)
+hola rafa
+>>> print("hola",nombre\ln)
+SyntaxError: unexpected character after line continuation character
+>>> 
+>>> print("hola",nombre)
+hola rafa
+>>> print("tu edad es", edad)
+tu edad es 18
+>>> print("hola",nombre,"tu edad es",edad)
+hola rafa tu edad es 18
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 15
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 18
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 18
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 18
+com se llama?
+rafa
+Me alegro,{nombree}
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 18
+com se llama?
+rafa
+Me alegro,{nombree}
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 18
+com se llama?
+d
+Me alegro,{nom}
+>>> ================================ RESTART ================================
+>>> 
+hola rafa tu edad es 18
+com se llama?
+r
+Me alegro r
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Users/PC12/Desktop/hh/rr.py", line 15, in <module>
+    print(nombre,apellido,carrera,edad,estarura,cumpleaños)
+NameError: name 'estarura' is not defined
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Users/PC12/Desktop/hh/rr.py", line 15, in <module>
+    print("hola",nombre,apellido,carrera,edad,estarura,cumpleaños)
+NameError: name 'estarura' is not defined
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+t
+cual es tu nombre??
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+rafa
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+rafa
+apellido??
+bermudez
+que estudias??
+ico
+cuantos años tienes??
+18
+cuanto mides??
+1.89
+dias de cumpleaños ??
+16
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+rafa
+apellido??
+bermudez
+que estudias??
+ico
+cuantos años tienes??
+18
+cuanto mides??
+1.85
+dias de cumpleaños ??
+16
+dias de cumpleaños ??
+mayo
+mes de cumpleaños ??
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+rafa
+apellido??
+bermudez
+que estudias??
+18
+cuantos años tienes??
+18
+cuanto mides??
+18
+dias de cumpleaños ??
+91
+dias de cumpleaños ??
+9
+mes de cumpleaños ??
+Tu nombre es  rafa tienes 18 años,estudias 18
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+rafa
+apellido??
+ber
+que estudias??
+ico
+cuantos años tienes??
+18
+cuanto mides??
+1.80
+dias de cumpleaños ??
+16
+dias de cumpleaños ??
+MAYO
+mes de cumpleaños ??
+Tu nombre es  rafa tienes 18 años,estudias ico cumples años el 16 MAYO y mides 1.80 mts
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+1
+apellido??
+74
+que estudias??
+568
+cuantos años tienes??
+5689
+cuanto mides??
+569
+dias de cumpleaños ??
+579
+dias de cumpleaños ??
+5790
+mes de cumpleaños ??
+Tu nombre es  1 tienes 5689 años,estudias 568 cumples años el 579 5790 y mides 569 mts
+>>> ================================ RESTART ================================
+>>> 
+cual es tu nombre??
+ts
+apellido??
+ts
+que estudias??
+tys
+cuantos años tienes??
+ys
+Traceback (most recent call last):
+  File "C:/Users/PC12/Desktop/hh/rr.py", line 22, in <module>
+    edad=int (input())
+ValueError: invalid literal for int() with base 10: 'ys'
+>>> 
---
+++ hh/rr.py
@@ -0,0 +1,35 @@
+#uno#
+"""edad='18'
+nombre='rafa'
+print("hola",nombre,"tu edad es",edad)
+print("com se llama?")
+nom=input()
+print("Me alegro",nom)"""
+#dos#
+"""print("que estudias??")carrera='Ingenieria en computacion'
+print("cuantos años tienes??")edad='18'
+print("cuanto mides??")estatura='1.85'
+print("dias de cumpleaños ??")cumpleaños=' 16'"""
+
+
+print("cual es tu nombre??")
+nombre=input()
+print("apellido??")
+apellido=input()
+print("que estudias??")
+carrera=input()
+print("cuantos años tienes??")
+edad=int (input())
+print("cuanto mides??")
+estatura=input()
+print("dias de cumpleaños ??")
+cumpleaños=input()
+print("dias de cumpleaños ??")
+mes=input()
+print("mes de cumpleaños ??")
+print("Tu nombre es ",nombre,"tienes",edad,"años,estudias",carrera,"cumples años el",cumpleaños,mes,"y mides",estatura,"mts")
+
+
+
+
+
